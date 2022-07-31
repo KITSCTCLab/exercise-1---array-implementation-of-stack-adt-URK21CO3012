329f16650ad3bc559c86a4b0e2561b810b8295b5
URK21CO3012 <110015521+URK21CO3012@users.noreply.github.com>
2022-07-31 09:34:04 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,30 +1,47 @@
 import os
+
 class Stack:
+
     def __init__(self, size):
+
         self.items = []
+
         self.size = size
+
         self.top = -1
 
     def is_empty(self):
+
         return self.top == -1
 
     def is_full(self):
+
         return self.top == self.size - 1
 
     def push(self, data):
+
         if not self.is_full():
+
             self.top += 1
+
             self.items.append(data)
 
     def pop(self):
+
         if not self.is_empty():
+
             x = self.items.pop(self.top)
+
             self.top -= 1
+
             return x
 
     def status(self):
+
         for item in self.items:
+
             print(item)
+
 
 
 # Do not change the following code
