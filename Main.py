dfb1cc8615ef2665e060eb6b5765f39c19c8942f
URK21CO3012 <110015521+URK21CO3012@users.noreply.github.com>
2022-07-31 09:27:23 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -40,3 +40,13 @@ for line in range(queries):
     elif values[0] == 2:
         stack.pop()
 stack.status()
+# Do not change the following code
+size, queries = map(int, input().rstrip().split())
+stack = Stack(size)
+for line in range(queries):
+    values = list(map(int, input().rstrip().split()))
+    if values[0] == 1:
+        stack.push(values[1])
+    elif values[0] == 2:
+        stack.pop()
+stack.status()
